5cd4bc3310ea4e1e45741e56fbeb01e68749dfb8
Rowan <r.kooij17@gmail.com>
2022-12-19 16:31:10 +0100

    keuzemenu met loop

--- AsyncShowcase/Program.cs
+++ AsyncShowcase/Program.cs
@@ -4,13 +4,60 @@
     {
         static async Task Main(string[] args)
         {
+            var loop = true;
+            while (loop)
+            {
+                Console.WriteLine("1. Sync vs Async");
+
+                Console.WriteLine("Exit? Type E");
+                Console.WriteLine("Choose one of the options");
+                var chosenOption = Console.ReadLine();
+
+                if (int.TryParse(chosenOption, out var choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            await SyncVsAsync();
+                            break;
+                        default:
+                            WrongChoice();
+                            break;
+                    }
+
+                    Console.WriteLine(); // One enter for next loop
+                }
+                else if (chosenOption?.ToLower() == "e")
+                {
+                    loop = false;
+                }
+                else
+                {
+                    WrongChoice();
+                }
+            }
+        }
+
+        private static async Task SyncVsAsync()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Making breakfast sync");
+            Console.ResetColor();
             await RealWorldExample.MakeBreakFast();
 
             Console.WriteLine();
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Making breakfast async");
+            Console.ResetColor();
             await RealWorldExample.MakeBreakFastAsync();
         }
+
+        private static void WrongChoice()
+        {
+            Console.WriteLine("Chosen option is not found try again");
+            Console.WriteLine();
+        }
     }
+
 }
