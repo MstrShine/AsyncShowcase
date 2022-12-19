dec181e33e784a25932b701ce6f8a84b165680d7
Rowan <r.kooij17@gmail.com>
2022-12-19 15:39:12 +0100

    Add project files.

---
+++ AsyncShowcase/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AsyncShowcase
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Making breakfast sync");
+            await RealWorldExample.MakeBreakFast();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Making breakfast async");
+            await RealWorldExample.MakeBreakFastAsync();
+        }
+    }
+}
---
+++ AsyncShowcase/RealWorldExample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncShowcase
+{
+    public static class RealWorldExample
+    {
+        public static async Task MakeBreakFast()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Starting toast!");
+            await MakeToast();
+            Console.WriteLine("Toast is done!");
+
+            Console.WriteLine("Making eggs!");
+            await BakeEggs();
+
+            stopwatch.Stop();
+            Console.WriteLine($"I have toast and eggs! Done in {(stopwatch.ElapsedMilliseconds / 1000)} seconds");
+        }
+            
+        public static async Task MakeBreakFastAsync()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Starting toast!");
+            var toastTask = MakeToast();
+
+            Console.WriteLine("Making eggs!");
+            await BakeEggs();
+
+            Console.WriteLine("Eggs are done, is the toast done?");
+            await toastTask;
+
+            stopwatch.Stop();
+            Console.WriteLine($"I have toast and eggs! Done in {(stopwatch.ElapsedMilliseconds / 1000)} seconds");
+        }
+
+        private static async Task BakeEggs()
+        {
+            Console.WriteLine("Cracking eggs in pan");
+            await Task.Delay(15000);
+            Console.WriteLine("Eggs are nice and crispy");
+        }
+
+        private static Task MakeToast()
+        {
+            Console.WriteLine("Pushing the toaster button.");
+            var toasterTask = Task.Delay(10000);
+            Console.WriteLine("Pushed the toaster button!");
+
+            return toasterTask;
+        }
+    }
+}
